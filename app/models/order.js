acbef79b0f421a08825b19c69ff901f096e7f279
appleJax <appleJax@users.noreply.github.com>
2016-08-01 21:21:46 -0700

    Move computed property 'totalItems' to order.js model from 'cart-total' component. Delete 'cart-total' component. Add 'total' row to order.hbs table.

--- app/models/order.js
+++ app/models/order.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Object.extend({
-
+  totalItems: Ember.computed('this.items.@each.quantity', function() {
+    return this.items.reduce(function(acc, item) {
+      return Number(acc) + Number(item.quantity);
+    }, 0);
+  })
 });
